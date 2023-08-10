392311a26abb73d57de94a9a9396c3d1aa0b697d
Sandru Gabriel <gabi200611@yahoo.com>
2023-08-10 22:31:10 +0300

    done

---
+++ EventBook/Mockups/MockupSign.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace EventBook.Mockups
+{
+    partial class MockupSign
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblTitlu = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSign = new System.Windows.Forms.Button();
+            this.txtRePass = new System.Windows.Forms.TextBox();
+            this.lblRePass = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 11.2F);
+            this.lblLogin.Location = new System.Drawing.Point(330, 597);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(232, 25);
+            this.lblLogin.TabIndex = 19;
+            this.lblLogin.Text = "Do you have an account?";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(424, 244);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(307, 38);
+            this.txtPass.TabIndex = 18;
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Location = new System.Drawing.Point(506, 193);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(112, 30);
+            this.lblPass.TabIndex = 17;
+            this.lblPass.Text = "Password";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(23, 244);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(307, 38);
+            this.txtName.TabIndex = 16;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(128, 192);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(76, 30);
+            this.lblName.TabIndex = 15;
+            this.lblName.Text = "Name";
+            // 
+            // lblTitlu
+            // 
+            this.lblTitlu.AutoSize = true;
+            this.lblTitlu.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 25.2F);
+            this.lblTitlu.Location = new System.Drawing.Point(291, 38);
+            this.lblTitlu.Name = "lblTitlu";
+            this.lblTitlu.Size = new System.Drawing.Size(171, 55);
+            this.lblTitlu.TabIndex = 14;
+            this.lblTitlu.Text = "Sign Up";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(758, 149);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnSign
+            // 
+            this.btnSign.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSign.Location = new System.Drawing.Point(263, 519);
+            this.btnSign.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.btnSign.Name = "btnSign";
+            this.btnSign.Size = new System.Drawing.Size(199, 71);
+            this.btnSign.TabIndex = 12;
+            this.btnSign.Text = "Sign Up";
+            this.btnSign.UseVisualStyleBackColor = true;
+            // 
+            // txtRePass
+            // 
+            this.txtRePass.Location = new System.Drawing.Point(229, 386);
+            this.txtRePass.Name = "txtRePass";
+            this.txtRePass.Size = new System.Drawing.Size(307, 38);
+            this.txtRePass.TabIndex = 21;
+            // 
+            // lblRePass
+            // 
+            this.lblRePass.AutoSize = true;
+            this.lblRePass.Location = new System.Drawing.Point(278, 332);
+            this.lblRePass.Name = "lblRePass";
+            this.lblRePass.Size = new System.Drawing.Size(201, 30);
+            this.lblRePass.TabIndex = 20;
+            this.lblRePass.Text = "Confirm Password";
+            // 
+            // MockupSign
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 654);
+            this.Controls.Add(this.txtRePass);
+            this.Controls.Add(this.lblRePass);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblTitlu);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSign);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "MockupSign";
+            this.Text = "MockupSign";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblTitlu;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSign;
+        private System.Windows.Forms.TextBox txtRePass;
+        private System.Windows.Forms.Label lblRePass;
+    }
+}
---
+++ EventBook/Mockups/MockupStart.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace EventBook.Mockups
+{
+    partial class MockupStart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblTitlu = new System.Windows.Forms.Label();
+            this.btnSign = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(201, 247);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(218, 86);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(687, 149);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblTitlu
+            // 
+            this.lblTitlu.AutoSize = true;
+            this.lblTitlu.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 18.2F);
+            this.lblTitlu.Location = new System.Drawing.Point(100, 53);
+            this.lblTitlu.Name = "lblTitlu";
+            this.lblTitlu.Size = new System.Drawing.Size(480, 40);
+            this.lblTitlu.TabIndex = 2;
+            this.lblTitlu.Text = "EventBook: Rezervări Simplificate";
+            // 
+            // btnSign
+            // 
+            this.btnSign.Location = new System.Drawing.Point(201, 411);
+            this.btnSign.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.btnSign.Name = "btnSign";
+            this.btnSign.Size = new System.Drawing.Size(218, 86);
+            this.btnSign.TabIndex = 3;
+            this.btnSign.Text = "Sign Up";
+            this.btnSign.UseVisualStyleBackColor = true;
+            // 
+            // MockupStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 35F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 622);
+            this.Controls.Add(this.btnSign);
+            this.Controls.Add(this.lblTitlu);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnLogin);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "MockupStart";
+            this.Text = "MockupStart";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblTitlu;
+        private System.Windows.Forms.Button btnSign;
+    }
+}
